2f7581475625fa633fcbdd739f2cc53ffb9f3f30
samsul <ms.arifin29@gmail.com>
2024-09-11 10:41:23 +0800

    fix : astro convig

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,18 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+
 import partytown from "@astrojs/partytown";
 
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ms-arifin.vercel.app',
   integrations: [mdx(), sitemap(), tailwind(), partytown()],
+
+  markdown: {
+    // @ts-ignore
+    extendDefaultPlugins: true,
+  },
+
 });
