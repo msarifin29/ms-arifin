ed2306bee58e388c3b2e04e350c6bca2d68ec610
samsul <ms.arifin29@gmail.com>
2024-09-11 10:38:32 +0800

    fix : astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,25 +3,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-import { autoNewTabExternalLinks } from './src/autoNewTabExternalLinks';
-
 import partytown from "@astrojs/partytown";
 
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ms-arifin.vercel.app',
   integrations: [mdx(), sitemap(), tailwind(), partytown()],
-
-  markdown: {
-    // @ts-ignore
-    extendDefaultPlugins: true,
-    // rehypePlugins: [[autoNewTabExternalLinks, {
-    //   domain: 'localhost:4321'
-    // }]]
-  },
-
-  output: 'server',
-  adapter: vercel()
 });
