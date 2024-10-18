d1e9fbc0e0cded3720295e2b5449035a1fdd3553
samsul <ms.arifin29@gmail.com>
2024-10-18 10:13:39 +0800

    feat : add tech stack on projects

--- src/pages/projects/projects.ts
+++ src/pages/projects/projects.ts
@@ -4,6 +4,7 @@ export interface Project {
   name: string;
   demoLink: string;
   tags?: string[],
+  techStack?: string[],
   description?: string;
   postLink?: string;
   demoLinkRel?: string;
@@ -16,23 +17,28 @@ export const projects: Project[] = [
     name: 'Randu Marketing Team',
     description: 'Payroll and HR Management Information System',
     demoLink: 'https://play.google.com/store/apps/details?id=com.rsm.randu_sales_mobile',
-    tags: ['HRIS', 'Saas']
+    tags: ['HRIS', 'Saas'],
+    techStack: ['Flutter']
   },
   {
     name: 'Jobseeker.life',
     description: 'Payroll and HR Management Information System',
     demoLink: 'https://play.google.com/store/apps/details?id=com.jobseeker.life',
-    tags: ['HRIS', 'Saas']
+    tags: ['HRIS', 'Saas'],
+    techStack: ['Flutter', 'Firebase']
   },
   {
     name: 'Jobseeker.app',
     description: 'Simplifying social media for job inspiration through TikTok',
     demoLink: 'https://play.google.com/store/apps/details?id=com.jobseeker.app',
-    tags: ['Bussiness']
+    tags: ['Bussiness'],
+    techStack: ['Flutter', 'Firebase']
   },
   {
     name: 'Movie Nominations',
+    description: 'Movie nomination using IMDb API to display favorite films',
     demoLink: 'https://github.com/msarifin29/fugi_movie_app_team5',
-    tags: ['Hobby']
+    tags: ['Hobby'],
+    techStack: ['Flutter']
   }
 ]
